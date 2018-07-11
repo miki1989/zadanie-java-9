3025172adc318b444170ffa0e4fe3b1c816f8255
miki1989 <mikolajheller@gmail.com>
2018-07-11 13:55:07 +0200

    Task java 9 - table with 3 values. Added 1,3 and 5 value.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int [] tablica = new int[] {1,2,100,4,200};
+       int result = tablica[0] + tablica[2] + tablica[4];
+        System.out.println(result);
+
+    }
+}
